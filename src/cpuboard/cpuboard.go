8eefa1553576fc9926c2b3afd85e3f4e567637af
0219Angry <angry.yuma.0219@gmail.com>
2024-05-31 16:08:57 +0900

    命令解読のSwitch

--- src/cpuboard/cpuboard.go
+++ src/cpuboard/cpuboard.go
@@ -41,6 +41,45 @@ const RUN_STEP = 1
  * ============================================================================*/
 func Step(cpub *Cpub) int {
 	/* Add any code for simulation of single instruction */
+	var (
+		mar Uword
+		ir  Uword
+	)
+
+	/*
+	 *	Phase 0 : Instruction Fetch
+	 */
+	mar = cpub.Pc
+	cpub.Pc++
+
+	/*
+	 *	Phase 1 : Instruction Fetch
+	 */
+	ir = cpub.Mem[mar]
+
+	/*
+	 *	Phase 2 : Instruction decode
+	 */
+	switch (ir & 0x0f) >> 4 {
+	case 0x0: /* NOP or HLT or JAL or JR */
+
+	case 0x1: /* OUT or IN */
+	case 0x2: /* RCF or SCF */
+	case 0x3: /* Bbc */
+	case 0x4: /* Ssm or Rsm */
+	case 0x5: /* empty */
+	case 0x6: /* LD */
+	case 0x7: /* ST */
+	case 0x8: /* SBC */
+	case 0x9: /* ADC */
+	case 0xa: /* SUB */
+	case 0xb: /* ADD */
+	case 0xc: /* EOR */
+	case 0xd: /* OR */
+	case 0xe: /* AND */
+	case 0xf: /* CMP */
+
+	}
 
 	return RUN_HALT
 }
